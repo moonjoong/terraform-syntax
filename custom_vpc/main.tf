3a5e7bfa692684e44d109ce43340ad4baa92c432
ofmw <ofmw91@gmail.com>
2024-03-08 10:29:13 +0900

    6

--- custom_vpc/main.tf
+++ custom_vpc/main.tf
@@ -4,10 +4,28 @@ resource "aws_vpc" "default" {
 
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.1.0/24"
+  count             = var.env == "dev" ? 1 : 0
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = ap-east-1a
+  tags = {
+    Name = "public_subnet_${var.env}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = ap-east-1a
+  tags = {
+    Name = "private_subnet_${var.env}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default.id
 
   tags = {
-    Name = "public_subnet"
+    Name = "hagaramit_igw${var.env}"
   }
 }
