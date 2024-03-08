6d8387c676f9a7eaeaf595715fdfd04e5c7f3eb0
ofmw <ofmw91@gmail.com>
2024-03-08 10:10:50 +0900

    2

--- main.tf
+++ main.tf
@@ -16,3 +16,13 @@ provider "aws" {
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
 }
+
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
