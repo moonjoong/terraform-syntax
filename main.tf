65c14f218657cd4b4a4c94a6a36b5f00252a12e8
ofmw <ofmw91@gmail.com>
2024-03-08 15:02:38 +0900

    10

--- main.tf
+++ main.tf
@@ -5,7 +5,16 @@ terraform {
       version = "~> 5.39.1"
     }
   }
+
+  backend "s3" {
+    bucket = "tf-backend-06-20240308"
+    key    = "terraform.tfstate"
+    region = "ap-east-1"
+    # dynamodb_table = "terraform-lock"
+  }
 }
+
+
 
 # Configure the AWS Provider
 provider "aws" {
@@ -56,3 +65,43 @@ resource "aws_s3_bucket_versioning" "tf_backend_versioning" {
     status = "Enabled"
   }
 }
+
+#	16.162.78.82
+# resource "aws_eip" "eip_test" {
+
+#   provisioner "local-exec" {
+#     command = "echo ${aws_eip.eip_test.public_ip}"
+#   }
+#   tags = {
+#     Name = "Test"
+
+#   }
+# }
+resource "aws_instance" "web-ec2" {
+  ami           = "ami-047c2b27e29f3093e"
+  instance_type = "t3.micro"
+  key_name      = "moonkey"
+  tags = {
+    Name = "my-00-web"
+  }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("moonkey.pem")
+    host        = self.public_ip
+
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt -y install nginx",
+      "sudo systemctl start nginx"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip}"
+  }
+}
+
+
